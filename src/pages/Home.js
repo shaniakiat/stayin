5f86051498b627fd9dc9bbc1316e6d1836fac190
shaniakiat <kiat.shania@gmail.com>
2019-08-03 16:33:05 -0400

    services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,21 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+import Services from "../components/Services";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <Hero>
-      <Banner title="Luxurious rooms" subtitle="deluxe room starting at $299">
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner title="Luxurious rooms" subtitle="deluxe room starting at $299">
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
