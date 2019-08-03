552e62766ba9e95d06b71534806a106dc66293db
shaniakiat <kiat.shania@gmail.com>
2019-08-03 16:07:37 -0400

    logo and banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Hello from home page</div>;
+  return (
+    <Hero>
+      <Banner title="Luxurious rooms" subtitle="deluxe room starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Home;
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Rooms = () => {
-  return <div>Hello from rooms page</div>;
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/rooms" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Rooms;
